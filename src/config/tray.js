a0f749e967167163b66235b73394acd858a752ca
Pedro <pedro.souza@viacometa.com.br>
2023-02-27 13:40:59 -0400

    test update

--- src/config/tray.js
+++ src/config/tray.js
@@ -4,7 +4,7 @@ var tray = null;
 export default function initTray(win) {
     const contextMenu = Menu.buildFromTemplate([
         {
-            label: 'Abrir Painel', click: function () {
+            label: 'Abrir Painel 42', click: function () {
                 if (!win.isVisible()) win.show()
             }
         },
